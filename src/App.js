09254ff31fa2d94dfcc3e441cbf986dad3a9e993
AleksandrKompanetc <moneymacker7@gmail.com>
2024-09-21 22:10:59 +0300

    adding functions edit and update task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [task, setTask] = useState('');
   const [todos, setTodos] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTask, setCurrentTask] = useState({});
 
   const addTask = () => {
     if (task.trim()) {
@@ -17,22 +19,45 @@ function App() {
     setTodos(newTodos);
   }
 
+  const editTask = (index) => {
+    setIsEditing(true);
+    setCurrentTask({index, text: todos[index] })
+  }
+
+  const updateTask = () => {
+    const updatedTodos = todos.map((todo, i) => 
+    i === currentTask.index ? currentTask.text : todo
+  );
+    setTodos(updatedTodos);
+    setIsEditing(false);
+    setCurrentTask({});
+  }
+
   return (
     <div className="App">
       <h1>Todo List</h1>
       <input 
         type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
+        value={isEditing ? currentTask.text : task}
+        onChange={(e) => 
+          isEditing 
+            ? setCurrentTask({...currentTask, text: e.target.value})
+            : setTask(e.target.value)
+        }
         placeholder='Enter task...'
       />
-      <button onClick={addTask}>Add Task</button>
+      {isEditing ? (
+        <button onClick={updateTask}>Update Task</button>
+      ) : (
+        <button onClick={addTask}>Add Task</button>
+      )}
 
       <ul>
         {todos.map((todo, index) => (
           <li key={index} className='todo'>
             {todo}
             <button onClick={() => deleteTask(index)} className='delete-btn'>Delete</button>
+            <button onClick={() => editTask(index)}>Edit</button>
           </li>
         ))}
       </ul>
